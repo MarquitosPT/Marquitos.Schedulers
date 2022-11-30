634845636616c66f42a79637b062df311149d102
Marcos Gomes <marquitospt@hotmail.com>
2022-11-30 09:40:49 +0000

    Added ScheduledTask Options Extension

---
+++ src/Marquitos.Schedulers/Extensions/ScheduledTaskOptionsExtension.cs
@@ -0,0 +1,27 @@
+﻿namespace Marquitos.Schedulers.Extensions
+{
+    /// <summary>
+    /// ScheduledTask Options Extension
+    /// </summary>
+    public static class ScheduledTaskOptionsExtension
+    {
+        /// <summary>
+        /// Update current options from other source options
+        /// </summary>
+        /// <param name="options">This options</param>
+        /// <param name="sourceOptions">Source options</param>
+        /// <exception cref="ArgumentNullException">Throws an ArgumentNullException if sourceOptions is null.</exception>
+        public static void UpdateFrom(this ScheduledTaskOptions options, ScheduledTaskOptions sourceOptions)
+        {
+            if (sourceOptions == null)
+            {
+                throw new ArgumentNullException(nameof(sourceOptions));
+            }
+
+            options.BeginOn = sourceOptions.BeginOn;
+            options.EndOn = sourceOptions.EndOn;
+            options.Schedule = sourceOptions.Schedule;
+            options.IsEnabled = sourceOptions.IsEnabled;
+        }
+    }
+}
